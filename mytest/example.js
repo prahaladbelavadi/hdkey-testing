df25024e6a2eed394e590529b7aa3fe15f055490
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-04-14 20:00:24 +0530

    need to convert to base 58/hex

--- mytest/example.js
+++ mytest/example.js
@@ -1,25 +1,38 @@
 var HDKey = require('hdkey')
+
 var seed = 'a0c42a9c3ac6abf2ba6a9946ae83af18f51bf1c9fa7dacc4c92513cc4dd015834341c775dcd4c0fac73547c5662d81a9e9361a0aac604a73a321bd9103bce8af'
 
 
 function time1(){
       var hdkey = HDKey.fromMasterSeed(new Buffer(seed, 'hex'))
       console.log("time1",hdkey.privateExtendedKey)
-      // => 'xprv9s21ZrQH143K2SKJK9EYRW3Vsg8tWVHRS54hAJasj1eGsQXeWDHLeuu5hpLHRbeKedDJM4Wj9wHHMmuhPF8dQ3bzyup6R7qmMQ1i1FtzNEW'
+
       console.log("time1",hdkey.publicExtendedKey)
-      // => 'xpub661MyMwAqRbcEvPmRAmYndzERhyNux1GoHzHxgzVHMBFkCro3kbbCiDZZ5XabZDyXPj5mH3hktvkjhhUdCQxie5e1g4t2GuAWNbPmsSfDp2'
     }
 
 function time2(){
       var hdkey = HDKey.fromMasterSeed(new Buffer(seed, 'hex'))
       console.log("time2:",hdkey.privateExtendedKey)
-      // => 'xprv9s21ZrQH143K2SKJK9EYRW3Vsg8tWVHRS54hAJasj1eGsQXeWDHLeuu5hpLHRbeKedDJM4Wj9wHHMmuhPF8dQ3bzyup6R7qmMQ1i1FtzNEW'
+
       console.log("time2",hdkey.publicExtendedKey)
-      // => 'xpub661MyMwAqRbcEvPmRAmYndzERhyNux1GoHzHxgzVHMBFkCro3kbbCiDZZ5XabZDyXPj5mH3hktvkjhhUdCQxie5e1g4t2GuAWNbPmsSfDp2'
     }
 
+function time3(){
+  var key = 'xprvA2nrNbFZABcdryreWet9Ea4LvTJcGsqrMzxHx98MMrotbir7yrKCEXw7nadnHM8Dq38EGfSh6dqA9QWTyefMLEcBYJUuekgW4BYPJcr9E7j'
+  var hdkey = HDKey.fromExtendedKey(key)
+  console.log(key);
+  console.log("HD key from extended Private key",toString(hdkey,"hex"));
+}
 
+function time4(){
+  var key = 'xprvA2nrNbFZABcdryreWet9Ea4LvTJcGsqrMzxHx98MMrotbir7yrKCEXw7nadnHM8Dq38EGfSh6dqA9QWTyefMLEcBYJUuekgW4BYPJcr9E7j'
+  var hdkey = HDKey.fromExtendedKey(key)
+  console.log(key);
+  console.log("HD key from extended public key",);
+}
 
 
 time1()
 time2()
+time3()
+time4()
