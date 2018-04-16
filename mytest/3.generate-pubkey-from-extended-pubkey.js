814a3389b8fb44f39c9c18ab3e32968b70ff5f5b
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-04-16 16:30:18 +0530

    add seperate file for generating-pubkey-from-extended-pubkey.js

---
+++ mytest/3.generate-pubkey-from-extended-pubkey.js
@@ -0,0 +1,5 @@
+function fromExtendedPubKey(){
+  var key = 'xpub6FnCn6nSzZAw5Tw7cgR9bi15UV96gLZhjDstkXXxvCLsUXBGXPdSnLFbdpq8p9HmGsApME5hQTZ3emM2rnY5agb9rXpVGyy3bdW6EEgAtqt'
+  var hdkey = HDKey.fromExtendedKey(key)
+  console.log("Private key from Extended Private Key:",key);
+}
