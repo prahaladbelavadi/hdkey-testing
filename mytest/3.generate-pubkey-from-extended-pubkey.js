0943c79312444052f32b9187cfb6757a5e0326d3
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-04-16 18:57:19 +0530

    add examples for     3.generate-pubkey-from-extended-pubkey.js 	  4.convert-seed-to-json.js

--- mytest/3.generate-pubkey-from-extended-pubkey.js
+++ mytest/3.generate-pubkey-from-extended-pubkey.js
@@ -1,5 +1,9 @@
+var HDKey = require('hdkey')
+
 function fromExtendedPubKey(){
   var key = 'xpub6FnCn6nSzZAw5Tw7cgR9bi15UV96gLZhjDstkXXxvCLsUXBGXPdSnLFbdpq8p9HmGsApME5hQTZ3emM2rnY5agb9rXpVGyy3bdW6EEgAtqt'
   var hdkey = HDKey.fromExtendedKey(key)
   console.log("Private key from Extended Private Key:",key);
 }
+
+fromExtendedPubKey()
