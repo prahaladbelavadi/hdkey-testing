e447b9468b1fbd1b3db5ef7906b071038fa69533
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-04-18 21:11:39 +0530

    export priv key

--- mytest/example2.js
+++ mytest/example2.js
@@ -48,9 +48,17 @@ function signHash(){
   hdkey.sign(transaction)
 }
 
+function exportPrivKey(){
+  var seed = 'fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542'
+  var hdkey = HDKey.fromMasterSeed(Buffer.from(seed, 'hex'))
+  console.log(hdkey.toString("base58"))
+}
+
 // genExtendedKeyfromMasterSeed()
 // fromExtendedPrivKey()
 // fromExtendedPubKey()
 // convtoJson()
 // deriveFromPath()
-signHash()
+// signHash()
+
+exportPrivKey()
